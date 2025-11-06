348e0b28f1597fbaf939c209dde07a76d123c483
Saurav Baral Joy <159685421+saurav716@users.noreply.github.com>
2025-11-06 18:17:37 +0600

    Create B. Tournament.cpp

---
+++ B. Tournament.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, j, k;
+        cin >> n >> j >> k;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        int strength_j = a[j - 1];
+        int count_stronger = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] <= strength_j) {
+                count_stronger++;
+            }
+        }
+
+        if (count_stronger == n || 1 < k) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
