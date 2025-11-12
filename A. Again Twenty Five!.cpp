888915c985ea8e3917733b7e537f6fa08174bfb6
Saurav Baral Joy <159685421+saurav716@users.noreply.github.com>
2025-11-12 19:47:07 +0600

    Create A. Again Twenty Five!.cpp

---
+++ A. Again Twenty Five!.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    // For n >= 2, the last two digits of 5^n are always 25
+    cout << "25" << endl;
+
+    return 0;
+}
